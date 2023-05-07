93b7ff272cd096d24b2ea2e97e001bac0bab9789
hieu291101 <latrunghieuvt1@gmail.com>
2023-05-07 11:57:46 +0700

    update 7.5.2022 - update register

--- src/configs/API.js
+++ src/configs/API.js
@@ -22,6 +22,15 @@ export const authAPI = () => axios.create({
     }
 })
 
+export const exportAPI = () => axios.create({
+    baseURL: "http://127.0.0.1:8000/",
+    headers: {
+        "Content-Type": "text/csv; charset=utf-8-sig", 
+        "MIME-Type": "text/csv", 
+        "Content-Disposition": "attachment; filename=export_dataframe.csv"
+    }
+})
+
 export default axios.create({
     // baseURL: "http://latrunghieuvt1.pythonanywhere.com/"
     baseURL: "http://127.0.0.1:8000/",
